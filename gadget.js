7a029fcd389f428c8b9bf937c91ea2b932b78c43
Baha <bmansurov@wikimedia.org>
2018-02-02 15:34:12 -0500

    Show section recommendations in edit view

--- gadget.js
+++ gadget.js
@@ -1,83 +1,177 @@
-// The current version only supports category based section recommendations.
-
-// The script works only in the 'view' action. To see the
-// recommendations in the 'edit' action, the article sections need to be
-// fetched from RESTBase (see TODO below).
-
-(function(d, $, mw) {
+/**
+ * The current version only supports category based section
+ * recommendations.
+ */
+(function(d, $, mw, ve) {
     var API_BASE_URL = 'http://gapfinder.wmflabs.org/en.wikipedia.org/v1/section/category/',
-        MAX_RECOMMENDATION_COUNT = 5;  // # of recommendations to show
+        // # of recommendations to show
+        MAX_RECOMMENDATION_COUNT = 5,
+        $recommendationsPlaceholder;
 
     // Must be the Main namespace
-    if (!mw.config.get('wgIsArticle') ||
-        mw.config.get('wgNamespaceNumber') !== 0) {
+    if (mw.config.get('wgNamespaceNumber') !== 0) {
         return;
     }
 
-    fetchSections(getCurrentArticleCategories(), showCategorySections);
+    if (mw.config.get('wgAction') === 'edit') {
+        showSectionRecommendations();
+    } else if (ve) {
+        ve.trackSubscribe('mwedit.loaded', showSectionRecommendations);
+        ve.trackSubscribe('mwedit.abort', hideSectionRecommendations);
+    }
+
+    /**
+     * Fetch and show missing section recommendations before the edit area.
+     */
+    function showSectionRecommendations() {
+        var title = mw.config.get('wgPageName');
+        getCategories(title)
+            .done(function (categories) {
+                if (categories) {
+                    $.when(getSectionRecommendations(categories),
+                           getSections(title))
+                        .then(getMissingSections)
+                        .then(showTopMissingSections);
+                }
+            });
+    }
+
+    /**
+     * Remove section recommendations from the page
+     */
+    function hideSectionRecommendations() {
+        $recommendationsPlaceholder.remove();
+    }
+
+    /**
+     * Show top missing sections
+     * @param {Array<Array<string, number>>} sections Array of section
+     * names and their relevance scores
+     */
+    function showTopMissingSections(sections) {
+        var $recommendationsPlaceholder =  $('<div id="gapfinder" class="mw-body">'),
+            topSections = sections.slice(0, MAX_RECOMMENDATION_COUNT);
+
+        $('<p>Here are the most relevant sections that are missing from the article you are editing. Feel free to create them.</p>')
+            .appendTo($recommendationsPlaceholder);
+
+        $('<ol></ol>').append(
+            $.map(topSections, function (section) {
+                return '<li>' + getNormalizedText(section[0]) + '</li>';
+            })
+        ).appendTo($recommendationsPlaceholder);
+
+        $recommendationsPlaceholder.insertBefore('#content');
+    }
+
+
+    /**
+     * Get missing sections
+     * @param {Object} sectionRecommendations
+     * @param {string[]} acrticleSections
+     * @return {Array<Array<string, number>>} missing sections
+     */
+    function getMissingSections (sectionRecommendations, articleSections) {
+        var result = [];
+
+        $.each(sectionRecommendations, function(name, score) {
+            if ($.inArray(name, articleSections) === -1) {
+                result.push([name, score]);
+            }
+        });
+
+        result.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        return result;
+    }
 
     /**
      * Return normalized title for text
      * Useful for normalizing categories and sections.
      * @param {string} text
-     * @returns {string} normalized text
+     * @return {string} normalized text
      */
     function getNormalizedTitle(text) {
         return mw.Title.newFromText(text).title;
     }
 
+    /**
+     * Return normalized text for title
+     * @param {string} title
+     * @return {string} normalized text
+     */
     function getNormalizedText(title) {
         return mw.Title.newFromText(title).getMainText();
     }
 
     /**
-     * Fetch sections of the current article
-     * @return {Set} normalized article sections
-     *
-     * TODO: Get this from restbase:
-     * https://en.wikipedia.org/api/rest_v1/#!/Mobile/get_page_mobile_sections_title_revision
+     * Get sections for title
+     * @param {string} title article title
+     * @return {jQuery.Promise}
+     * @return {Function} return.done
+     * @return {string[]|false} return.done.data normalized section
+     * names or false
      */
-    function getCurrentArticleSections() {
-        var headlines = $('.mw-headline');
-
-        return new Set($.map(headlines, function(headline) {
-            return getNormalizedTitle($(headline).text());
-        }));
+    function getSections(title) {
+        return new mw.Api()
+            .get({
+                action: 'parse',
+                page: getNormalizedTitle(title),
+                prop: 'sections'
+            })
+            .then(function(data) {
+                if (data && data.parse && data.parse.sections) {
+                    return $.map(data.parse.sections, function(section) {
+                        return getNormalizedTitle(section.line);
+                    });
+                }
+                return false;
+            });
     }
 
     /**
-     * Get current article categories
-     * @return {Array.string} Array of normalized category names
+     * Get categories for title
+     * @param {string} title article title
+     * @return {jQuery.Promise}
+     * @return {Function} return.done
+     * @return {string[]|false} return.done.data normalized category
+     * names or false
      */
-    function getCurrentArticleCategories() {
-        return $.map(mw.config.get('wgCategories'), function(category) {
-            return getNormalizedTitle(category);
+    function getCategories(title) {
+        return mw.loader.using('mediawiki.api.category').then(function () {
+            return new mw.Api().getCategories(title)
+                .then(function (categories) {
+                    if (categories) {
+                        return $.map(categories, function(category) {
+                            return getNormalizedTitle(category.title);
+                        });
+                    }
+                    return false;
+                });
         });
     }
 
     /**
-     * Fetch sections of category and call callback with data
-     * @param {Array.string} categories Category names that are missing
-     * from the current article
+     * Get section recommendations for categories
+     * @param {string[]} categories
+     * @return {jQuery.Promise}
+     * @return {Function} return.done
+     * @return {Object|false} return.done.data normalized section
+     * names and their relevance scores as key value pairs or false
      */
-    function fetchSections(categories, callback) {
-        var currentArticleSections = getCurrentArticleSections(),
+    function getSectionRecommendations(categories) {
+        var section,
             sections = {},
-            sectionsArray = [],
-            // TODO: make the service accept multiple categories
             requests = $.map(categories, function(category) {
                 return $.getJSON(API_BASE_URL + category);
             });
 
-        $.when.apply($, requests).then(function() {
+        return $.when.apply($, requests).then(function() {
             Array.from(arguments).forEach(function(response) {
                 response[0].forEach(function(apiSection) {
-                    var section = getNormalizedTitle(apiSection[0]);
-                    // Only consider sections that are missing from the
-                    // current article.
-                    if (currentArticleSections.has(section)) {
-                        return;
-                    }
+                    section = getNormalizedTitle(apiSection[0]);
                     if (section in sections) {
                         sections[section] =+ apiSection[1];
                     } else {
@@ -85,31 +179,8 @@
                     }
                 });
             });
-
-            $.each(sections, function(name, score) {
-                sectionsArray.push([name, score]);
-            });
-
-            sectionsArray.sort(function(a, b) {
-                return b[1] - a[1];
-            });
-
-            callback(sectionsArray.slice(0, MAX_RECOMMENDATION_COUNT));
+            return sections;
         });
     }
 
-    /**
-     * Show category sections
-     * @param {Array.Array<string, number>} sections Array of section
-     * names and their relevance scores
-     */
-    function showCategorySections(sections) {
-        // TODO: generate code color green to red using the relevance
-        // scores and use that color as the section background color.
-        $('<ul></ul>').append(
-            $.map(sections, function (section) {
-                return '<li>' + getNormalizedText(section[0]) + '</li>';
-            })
-        ).insertAfter('#firstHeading');
-    }
-}(document, jQuery, mediaWiki));
+}(document, window.jQuery, window.mediaWiki, window.ve));
